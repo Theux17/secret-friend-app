0a436381bc2b7d8c7db864aaa1bab159f6d05cfc
Matheus Silva <oliveira.ms18@gmail.com>
2022-09-29 21:19:42 -0300

    Remove header 'Access-Control-Allow-Origin'

--- index.js
+++ index.js
@@ -107,7 +107,6 @@ async function drawFriend(event) {
             await fetch("https://secret-friends-api.herokuapp.com/users", {
             method: "POST",
             headers: {
-                "Access-Control-Allow-Origin": "*",
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ users: users }),
